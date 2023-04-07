77f51cb7b3cb3ece73458f7d88cd855d14c486f3
Jonas Ehrlich <jonas.ehrlich@gmail.com>
2023-04-07 17:13:20 +0200

    Simplify utils test case

--- test/test_utils.py
+++ test/test_utils.py
@@ -1,4 +1,3 @@
-import io
 import pathlib
 import tempfile
 
@@ -8,44 +7,43 @@ from plantuml_sequence import utils
 
 
 def test_line_writer_writeline() -> None:
-    file_like = io.StringIO()
-    line_writer = utils.LineWriter(file_like)
-
     lines = ["hello", "world", "lorem ipsum"]
-    for line in lines:
-        line_writer.writeline(line)
+    with utils.string_io() as file_like:
+        line_writer = utils.LineWriter(file_like)
+        for line in lines:
+            line_writer.writeline(line)
 
-    file_like.seek(0)
     # Read lines from file-like, except the last, empty one
     output_lines = file_like.read().split("\n")[:-1]
     assert output_lines == lines
 
 
 def test_line_writer_writelines() -> None:
-    file_like = io.StringIO()
-    line_writer = utils.LineWriter(file_like)
-
     lines = ["hello", "world", "lorem ipsum"]
-    line_writer.writelines(lines)
-    file_like.seek(0)
+    with utils.string_io() as file_like:
+        line_writer = utils.LineWriter(file_like)
+        line_writer.writelines(lines)
+
     # Read lines from file-like, except the last, empty one
     output_lines = file_like.read().split("\n")[:-1]
     assert output_lines == lines
 
 
 def test_line_writer_indent() -> None:
-    file_like = io.StringIO()
-    line_writer = utils.LineWriter(file_like)
-    lines = ["hello", "world", "lorem ipsum"]
-    expected_output = [" " * num_spaces + item for num_spaces, item in zip((2, 4, 2), lines, strict=True)]
-    lines_iter = iter(lines)
 
-    with line_writer.indent():
+    lines = ["123", "hello", "world", "lorem ipsum", "zero"]
+    expected_output = [" " * num_spaces + item for num_spaces, item in zip((0, 2, 4, 2, 0), lines, strict=True)]
+    lines_iter = iter(lines)
+    with utils.string_io() as file_like:
+        line_writer = utils.LineWriter(file_like)
         line_writer.writeline(next(lines_iter))
         with line_writer.indent():
             line_writer.writeline(next(lines_iter))
+            with line_writer.indent():
+                line_writer.writeline(next(lines_iter))
+            line_writer.writeline(next(lines_iter))
         line_writer.writeline(next(lines_iter))
-    file_like.seek(0)
+
     # Read lines from file-like, except the last, empty one
     output_lines = file_like.read().split("\n")[:-1]
     assert output_lines == expected_output
