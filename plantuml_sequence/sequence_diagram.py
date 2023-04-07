b56a380c60590db47df9d79abee2925b06ce0dbe
Jonas Ehrlich <jonas.ehrlich@gmail.com>
2023-04-07 17:09:33 +0200

    Add method to declare participants

--- plantuml_sequence/sequence_diagram.py
+++ plantuml_sequence/sequence_diagram.py
@@ -1,6 +1,8 @@
+import collections.abc
 import dataclasses
+import functools
 from types import TracebackType
-from typing import Literal, Self, TextIO, Type, TypeAlias, TypeVar
+from typing import Literal, ParamSpec, Self, TextIO, Type, TypeAlias, TypeVar
 
 from . import utils
 
@@ -12,23 +14,38 @@ GroupType: TypeAlias = Literal["alt", "else", "opt", "loop", "par", "break", "cr
 
 
 T = TypeVar("T", bound=TextIO)
+P = ParamSpec
 
 
 @dataclasses.dataclass(frozen=True)
 class Participant:
     title: str
     shape: ParticipantShape
+    alias: str = ""
     color: str = ""
 
-    def key(self) -> str:
-        return self.title.replace(" ", "_").replace("-", "_").lower()
+    def __post_init__(self) -> None:
+        if not self.alias:
+            object.__setattr__(self, "alias", "".join(filter(str.isalnum, self.title)))
 
     def __str__(self) -> str:
-        return f'{self.shape} "{self.title}" as {self.key()} {self.color}'
+        quoted_title = quote_string_if_required(self.title)
+        return f"{self.shape} {quoted_title} as {self.alias} {self.color}"
 
 
-def title_to_key(title: str):
-    return title.replace(" ", "_").replace("-", "_").lower()
+def _participant_shape_partialmethod(
+    generic_declare_method: collections.abc.Callable[..., Participant], shape: ParticipantShape
+) -> functools.partialmethod[Participant]:
+    """Create a :py:class:`functools.partialmethod` object with a fixed shape argument"""
+    new_method = functools.partialmethod(generic_declare_method, shape=shape)
+    if generic_declare_method.__doc__ is not None:
+        new_docstring = "\n".join(
+            line.replace("participant", shape)
+            for line in generic_declare_method.__doc__.split("\n")
+            if "shape" not in line
+        )
+        new_method.__doc__ = new_docstring
+    return new_method
 
 
 class SequenceDiagram:
@@ -88,14 +105,53 @@ class SequenceDiagram:
         message_suffix = ""
         if message:
             message_suffix = f": {message}"
-        participant1 = participant_to_key(participant1)
-        participant2 = participant_to_key(participant2)
+        participant1 = participant_to_string(participant1)
+        participant2 = participant_to_string(participant2)
         arrow_style = arrow_style or self._arrow_style
         self._line_writer.writeline(f"{participant1} {arrow_style} {participant2}{message_suffix}")
         return self
 
+    def declare_participant(
+        self, title: str, shape: ParticipantShape = "participant", alias: str | None = None, color: str | None = None
+    ) -> Participant:
+        """
+        Declare a participant
 
-def participant_to_key(participant: Participant | str | None) -> str:
+        In order to reference this participant in the future use its :py:class:`Participant.alias` attribute.
+
+        :param title: Title of the participant
+        :type title: str
+        :param shape: Shape of the participant, defaults to "participant"
+        :type shape: ParticipantShape, optional
+        :param alias: Alias for the participant to use, defaults to ""
+        :type alias: str, optional
+        :param color: _description_, defaults to ""
+        :type color: str, optional
+        :raises ValueError: _description_
+        :return: _description_
+        :rtype: Participant
+        """
+        alias = alias or ""
+        color = color or ""
+        participant = Participant(title=title, shape=shape, alias=alias, color=color)
+
+        if participant.alias in self._participants:
+            raise ValueError(f"Participant with alias '{participant.alias}' already exists")
+
+        self._participants[participant.alias] = participant
+        self._line_writer.writeline(str(participant))
+        return participant
+
+    declare_actor = _participant_shape_partialmethod(declare_participant, shape="actor")
+    declare_boundary = _participant_shape_partialmethod(declare_participant, shape="boundary")
+    declare_control = _participant_shape_partialmethod(declare_participant, shape="control")
+    declare_entity = _participant_shape_partialmethod(declare_participant, shape="entity")
+    declare_database = _participant_shape_partialmethod(declare_participant, shape="database")
+    declare_collections = _participant_shape_partialmethod(declare_participant, shape="collections")
+    declare_queue = _participant_shape_partialmethod(declare_participant, shape="queue")
+
+
+def participant_to_string(participant: Participant | str | None) -> str:
     """
     Create a string representation of a participant
 
@@ -107,8 +163,19 @@ def participant_to_key(participant: Participant | str | None) -> str:
     if participant is None:
         return ""
     if isinstance(participant, Participant):
-        return participant.key()
+        return participant.alias
+    return quote_string_if_required(participant)
 
-    if participant.isalnum():
-        return participant
-    return f'"{participant}"'
+
+def quote_string_if_required(value: str):
+    """
+    Quote a string if it is not purely alphanumeric
+
+    :param value: String to quote
+    :type value: str
+    :return: Quoted or unquoted string
+    :rtype: _type_
+    """
+    if value.isalnum():
+        return value
+    return f'"{value}"'
--- test/test_examples.py
+++ test/test_examples.py
@@ -2,7 +2,7 @@ from plantuml_sequence import SequenceDiagram
 from plantuml_sequence.utils import string_io
 
 
-def test_basic_examples():
+def test_basic_examples() -> None:
     """Test creation of a simple message"""
     with string_io() as file_like, SequenceDiagram(file_like) as sequence:
         (
@@ -23,6 +23,46 @@ Bob --> Alice: Authentication Response
 
 Alice -> Bob: Another authentication Request
 Alice <-- Bob: Another authentication Response
+@enduml
+"""
+    )
+
+
+def test_declaring_participant() -> None:
+    with string_io() as file_like, SequenceDiagram(file_like) as sequence:
+        participants = [
+            sequence.declare_participant("Participant", alias="Foo"),
+            sequence.declare_actor("Actor", alias="Foo1"),
+            sequence.declare_boundary("Boundary", alias="Foo2"),
+            sequence.declare_control("Control", alias="Foo3"),
+            sequence.declare_entity("Entity", alias="Foo4"),
+            sequence.declare_database("Database", alias="Foo5"),
+            sequence.declare_collections("Collections", alias="Foo6"),
+            sequence.declare_queue("Queue", alias="Foo7"),
+        ]
+        for dest in participants[1:]:
+            sequence.message(participants[0], dest, f"To {dest.shape}")
+
+    content = file_like.read()
+    assert (
+        content
+        == """\
+@startuml
+participant Participant as Foo
+actor Actor as Foo1
+boundary Boundary as Foo2
+control Control as Foo3
+entity Entity as Foo4
+database Database as Foo5
+collections Collections as Foo6
+queue Queue as Foo7
+Foo -> Foo1: To actor
+Foo -> Foo2: To boundary
+Foo -> Foo3: To control
+Foo -> Foo4: To entity
+Foo -> Foo5: To database
+Foo -> Foo6: To collections
+Foo -> Foo7: To queue
 @enduml
 """
     )
