f8f210dc98ea802eb291396b6fe72999ba5b48e7
Jonas Ehrlich <jonas.ehrlich@gmail.com>
2023-04-10 13:51:42 +0200

    Add documentation setup

---
+++ docs/conf.py
@@ -0,0 +1,36 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "plantuml-sequence"
+copyright = "2023, Jonas Ehrlich"
+author = "Jonas Ehrlich"
+release = "0.1.0"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "myst_parser",
+]
+templates_path = ["_templates"]
+exclude_patterns = []
+
+source_suffix = {
+    ".rst": "restructuredtext",
+    ".txt": "markdown",
+    ".md": "markdown",
+}
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "furo"
+html_static_path = ["_static"]
+
+# -- Options for myst-parser -------------------------------------------------
---
+++ docs/index.md
@@ -0,0 +1,12 @@
+
+# plantuml-sequence
+
+```{toctree}
+:maxdepth: 3
+```
+
+## Indices and tables
+
+* [Index](genindex)
+* [Modules](modindex)
+* [Search](search)
